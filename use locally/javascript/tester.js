c575c5598b69ade37625150b9ba9146b2b824226
Aiden Li <liaiden3918@gmail.com>
2022-03-02 16:41:49 -0800

    prevented keydown overflow

--- use locally/javascript/tester.js
+++ use locally/javascript/tester.js
@@ -2,13 +2,6 @@ for (let i=0; i<103; i++) {document.getElementsByClassName('key')[i].style.borde
 
 //press key down
 document.getElementById('body').addEventListener('keydown', e => {
-    if (e.code[0] == 'F' || e.metaKey || e.ctrlKey || e.altKey || e.key == 'Tab' || e.key == 'Enter' || e.key == 'CapsLock' || 
-    e.key == 'Shift' || e.key == 'ContextMenu' || e.code[0] == 'A' || e.key == 'Insert' || e.key == 'Home' || e.key == 'PageUp' ||
-    e.key == 'Delete' || e.key == 'End' || e.key == 'PageDown' || e.key == 'Escape' || 
-    /*key exception handling*/ e.key.length > 10) {
-        e.preventDefault();
-        document.getElementById('key-input').textContent += '[' + e.key + ']';
-    } else {document.getElementById('key-input').textContent += e.key;}
     try {
         if (document.getElementById(e.code).style.borderColor == 'gray') {
             document.getElementById(e.code).style.borderColor = 'rgb(103, 65, 105)'; 
@@ -24,6 +17,13 @@ document.getElementById('body').addEventListener('keyup', e => {
         document.getElementById(e.code).style.borderColor = 'green'; 
         document.getElementById(e.code).style.animation = 'keyUp 0.25s'; 
     }
+    if (e.code[0] == 'F' || e.metaKey || e.ctrlKey || e.altKey || e.key == 'Tab' || e.key == 'Enter' || e.key == 'CapsLock' || 
+    e.key == 'Shift' || e.key == 'ContextMenu' || e.code[0] == 'A' || e.key == 'Insert' || e.key == 'Home' || e.key == 'PageUp' ||
+    e.key == 'Delete' || e.key == 'End' || e.key == 'PageDown' || e.key == 'Escape' || 
+    /*key exception handling*/ e.key.length > 10) {
+        e.preventDefault();
+        document.getElementById('key-input').textContent += '[' + e.key + ']';
+    } else {document.getElementById('key-input').textContent += e.key;}
 })
 
 //reset button
